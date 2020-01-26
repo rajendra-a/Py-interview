74fa93feef586d597b8896c633e38ea7eb73eb37
raajendra9 <rajendrageek@gmail.com>
2020-01-26 17:02:15 +0530

    frequency of letter count

---
+++ frequency_letter_count.py
@@ -0,0 +1,24 @@
+def printRLEs(s):
+
+    i=0
+    while(i < len(s)-1):
+
+        # counting occurences
+        count = 1
+
+        while s[i] == s[i+1]:
+
+            i += 1
+            count += 1
+
+            if i + 1 == len(s):
+                break
+        
+        print(str(s[i])+ str(count), end=" ")
+
+        i += 1
+
+    print()
+
+
+printRLEs('raajendraa')
