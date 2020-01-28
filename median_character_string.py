8cc36cb19d8ef137aecbc8929f8eab3c930e4c87
raajendra9 <rajendrageek@gmail.com>
2020-01-28 13:31:22 +0530

    find the median character in string

---
+++ median_character_string.py
@@ -0,0 +1,9 @@
+def returnMedian(mystring):
+    return mystring[(len(mystring)-1)//2]
+    # length = len(mystring)
+    # median = (length-1)//2
+    # stringmedian = mystring[median]
+    # print(stringmedian)
+    # return mystring[median]
+
+returnMedian('rajendra')
